0b2768a210b918e3319cd1a046a112f703d14e20
mneedham01 <mneedham@smith.edu>
2025-02-16 22:26:47 -0500

    all images randomly rotate when u click anywhere in the contair

--- new load page/src.js
+++ new load page/src.js
@@ -1,8 +1,15 @@
-function random_rotate(){
+function call_random_rotate(){
+    ids = ["e_image", "m_image", "u_image", "l_image", "a_image", "t_image", "e1_image"]
+    for (var id of ids) {
+        random_rotate(id);
+    }
+}
+
+function random_rotate(id){
     // chooses between -30 and 30 degrees 
     const min = -30;
     const max = 30;
     var degree = Math.floor(Math.random() * (max - min + 1)) + min;
 
-    document.getElementById("e_image").style.transform = "rotate("+ degree.toString() + "deg)";
+    document.getElementById(id).style.transform = "rotate("+ degree.toString() + "deg)";
 }
