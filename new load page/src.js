8286df20d7c1dc63e2d5f494d367b33d55546140
buddy <kluong@smith.edu>
2025-02-24 16:30:28 -0500

    loading page updates!
    start with random tile on loading page, rotation changes on mouse over now, yayy!

--- new load page/src.js
+++ new load page/src.js
@@ -1,19 +1,19 @@
-setInterval(function call_random_rotate() {
+// setInterval(call_random_rotate, 1000);
+function call_random_rotate() {
     let ids = ["e_image", "m_image", "u_image", "l_image", "a_image", "t_image", "e1_image"]
     for (var id of ids) {
         random_rotate(id);
     }
-}, 1000);
-
+}
 function random_rotate(id){
     // chooses between -30 and 30 degrees 
-    const min = -30;
-    const max = 30;
+    const min = -25;
+    const max = 25;
     var degree = Math.floor(Math.random() * (max - min + 1)) + min;
 
     document.getElementById(id).style.transform = "rotate("+ degree.toString() + "deg)";
 }
-
+window.addEventListener("load", call_random_rotate);
 window.addEventListener("click", enterPage);
 
 function enterPage(){
