688e9654e3260e81726f19fc0abfbbcf8642cc4a
buddy <kluong@smith.edu>
2025-02-26 15:44:32 -0500

    added a bunch of letters

--- load page/src.js
+++ load page/src.js
@@ -1,32 +1,21 @@
-function highlight(){
-    document.getElementById("title").style.color = "darkred";
+// setInterval(call_random_rotate, 1000);
+function call_random_rotate() {
+    let ids = ["e_image", "m_image", "u_image", "l_image", "a_image", "t_image", "i_image", "n_image", "g_image"]
+    for (var id of ids) {
+        random_rotate(id);
+    }
 }
-function unhighlight(){
-    document.getElementById("title").style.color = "black";
+function random_rotate(id){
+    // chooses between -30 and 30 degrees 
+    const min = -25;
+    const max = 25;
+    var degree = Math.floor(Math.random() * (max - min + 1)) + min;
+
+    document.getElementById(id).style.transform = "rotate("+ degree.toString() + "deg)";
 }
-function exit(id){
-    document.getElementById(id).style.display = "none";
+window.addEventListener("load", call_random_rotate);
+window.addEventListener("click", enterPage);
+
+function enterPage(){
+    window.location.href = '../home page/home.html';
 }
-
-// window.addEventListener("click", function goHome(){
-//     window.open("../home page/home.html");
-// });
-
-//THIS IS NOT WORKING WHYYYY
-
-// window.addEventListener("load", function randomize(){
-//     var r;
-//     var list = document.getElementsByClassName("randomize")
-//     for(var i=0; i<36; i++){
-//         r = Math.floor(Math.random()*200);
-//         list[i].style.margin = r + "px";
-//     }
-// });
-// window.addEventListener("load", randomize(document.getElementById("welcome")));
-// window.addEventListener("load", randomize(document.getElementById("zine")));
-// const getRandom = (min, max) => Math.floor(Math.random()*(max-min+1)+min);
-// function randomize(ID){
-//     ID.style.left = getRandom(0, 300 - 200)+'px';
-//     ID.style.top = getRandom(0, 300 - 200)+'px';
-// }
-
