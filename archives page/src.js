ec413d7a939e49d71078d84127e8541a02c9919a
buddy <kluong@smith.edu>
2025-03-12 16:00:29 -0400

    flipbooks!!!

--- archives page/src.js
+++ archives page/src.js
@@ -1,41 +1,125 @@
-const move = document.getElementById("move");
+// References to DOM Elements
+const prevBtn = document.querySelector("#prev-btn");
+const nextBtn = document.querySelector("#next-btn");
+const book = document.querySelector("#book");
 
-function popup(iD){
-    var x = document.getElementById(iD);
-    if(x.style.display === "none"){
-        x.style.display = "block";
-    }else{
-        x.style.display = "none";
+const paper1 = document.querySelector("#p1");
+const paper2 = document.querySelector("#p2");
+const paper3 = document.querySelector("#p3");
+
+// Event Listener
+prevBtn.addEventListener("click", goPrevPage);
+nextBtn.addEventListener("click", goNextPage);
+
+// Business Logic
+let currentLocation = 1;
+let numOfPapers = 3;
+let maxLocation = numOfPapers + 1;
+
+function openBook() {
+    book.style.transform = "translateX(50%)";
+    prevBtn.style.transform = "translateX(-180px)";
+    nextBtn.style.transform = "translateX(180px)";
+}
+
+function closeBook(isAtBeginning) {
+    if(isAtBeginning) {
+        book.style.transform = "translateX(0%)";
+    } else {
+        book.style.transform = "translateX(100%)";
+    }
+
+    prevBtn.style.transform = "translateX(0px)";
+    nextBtn.style.transform = "translateX(0px)";
+}
+
+function goNextPage() {
+    if(currentLocation < maxLocation) {
+        switch(currentLocation) {
+            case 1:
+                openBook();
+                paper1.classList.add("flipped");
+                paper1.style.zIndex = 1;
+                break;
+            case 2:
+                paper2.classList.add("flipped");
+                paper2.style.zIndex = 2;
+                break;
+            case 3:
+                paper3.classList.add("flipped");
+                paper3.style.zIndex = 3;
+                closeBook(false);
+                break;
+            default:
+                throw new Error("unkown state");
+        }
+        currentLocation++;
     }
 }
-// document.getElementById("zine1").addEventListener("mouseover", hover("move"));
-// document.getElementById("zine1").addEventListener("mouseout", exit("move"));
 
-document.body.onpointermove = event => {
-    const { clientX, clientY } = event;
+function goPrevPage() {
+    if(currentLocation > 1) {
+        switch(currentLocation) {
+            case 2:
+                closeBook(true);
+                paper1.classList.remove("flipped");
+                paper1.style.zIndex = 3;
+                break;
+            case 3:
+                paper2.classList.remove("flipped");
+                paper2.style.zIndex = 2;
+                break;
+            case 4:
+                openBook();
+                paper3.classList.remove("flipped");
+                paper3.style.zIndex = 1;
+                break;
+            default:
+                throw new Error("unkown state");
+        }
 
-    move.animate({
-        left: `${clientX}px`,
-        top: `${clientY}px`
-
-    }, {duration: 1000, fill: "forwards"})
-
+        currentLocation--;
+    }
 }
-function hover(iD){
-    var x = document.getElementById(iD);
-    x.style.display = "block"
-}
 
-function exit(iD){
-    var x = document.getElementById(iD);
-    x.style.display = "none"
-}
-
-function openViewer(iD, elmtID){
-    var elmt = document.getElementById(elmtID)
-    var x = document.getElementById(iD);
-    x.style.display = "block"
-
-}
+// const move = document.getElementById("move");
+//
+// function popup(iD){
+//     var x = document.getElementById(iD);
+//     if(x.style.display === "none"){
+//         x.style.display = "block";
+//     }else{
+//         x.style.display = "none";
+//     }
+// }
+// // document.getElementById("zine1").addEventListener("mouseover", hover("move"));
+// // document.getElementById("zine1").addEventListener("mouseout", exit("move"));
+//
+// document.body.onpointermove = event => {
+//     const { clientX, clientY } = event;
+//
+//     move.animate({
+//         left: `${clientX}px`,
+//         top: `${clientY}px`
+//
+//     }, {duration: 1000, fill: "forwards"})
+//
+// }
+// function hover(iD){
+//     var x = document.getElementById(iD);
+//     x.style.display = "block"
+// }
+//
+// function exit(iD){
+//     var x = document.getElementById(iD);
+//     x.style.display = "none"
+// }
+//
+// function openViewer(iD, elmtID){
+//     var elmt = document.getElementById(elmtID)
+//     var x = document.getElementById(iD);
+//     x.style.display = "block"
+//
+// }
 
 
