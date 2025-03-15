fcde241d4792da4f6477b3583cb3feb135720d12
mneedham01 <mneedham@smith.edu>
2025-03-15 13:56:36 -0400

    added scissor animation

--- home page/src.js
+++ home page/src.js
@@ -1,5 +1,26 @@
+// testing accessing universal variables 
 function getLetterIds() {
     const object = localStorage.getItem("letterIds");
 
     window.alert(object["A"]);
 }
+
+// this function opens the scissors
+function open() {
+    let top = document.getElementById("top");
+    let bottom = document.getElementById("bottom");
+    top.animate (
+        {
+            transform: ["rotate(0deg)", "rotate(15deg)"]
+        }, 1000,
+    )
+    bottom.animate (
+        {
+            transform: ["rotate(0deg)", "rotate(-15deg)"]
+        }, 1000,
+    )
+    top.style.transform = "rotate(15deg)";
+    bottom.style.transform = "rotate(-15deg)";
+}
+
+window.addEventListener("load", open);
