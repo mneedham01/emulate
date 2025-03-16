bf2c5d7de8a57e39bb239b399bce3dc683e2b5f3
mneedham01 <mneedham@smith.edu>
2025-03-16 16:13:18 -0400

    got ride of doodles, added new emulate logo

--- home page/src.js
+++ home page/src.js
@@ -5,20 +5,39 @@ function getLetterIds() {
     window.alert(object["A"]);
 }
 
+let opened = "closed";
+
 // this function opens the scissors
 function openScissors() {
     let top = document.getElementById("top");
     let bottom = document.getElementById("bottom");
-    top.animate (
-        {
-            transform: ["rotate(0deg)", "rotate(10deg)"]
-        }, 1000,
-    )
-    bottom.animate (
-        {
-            transform: ["rotate(0deg)", "rotate(-10deg)"]
-        }, 1000,
-    )
-    top.style.transform = "rotate(10deg)";
-    bottom.style.transform = "rotate(-10deg)";
+    if (opened == "closed") {
+        top.animate (
+            {
+                transform: ["rotate(0deg)", "rotate(10deg)"]
+            }, 1000,
+        )
+        bottom.animate (
+            {
+                transform: ["rotate(0deg)", "rotate(10deg)"]
+            }, 1000,
+        )
+        top.style.transform = "rotate(10deg)";
+        bottom.style.transform = "rotate(10deg)";
+        opened = "opened";
+    } else {
+        top.animate (
+            {
+                transform: ["rotate(10deg)", "rotate(0deg)"]
+            }, 1000,
+        )
+        bottom.animate (
+            {
+                transform: ["rotate(10deg)", "rotate(0deg)"]
+            }, 1000,
+        )
+        top.style.transform = "rotate(0deg)";
+        bottom.style.transform = "rotate(0deg)";
+        opened = "closed";
+    }
 }
