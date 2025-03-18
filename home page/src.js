1ddf57ca1e2aaad457bc087143be9c088ddb8cd8
mneedham01 <mneedham@smith.edu>
2025-03-18 10:25:58 -0400

    current animation -- NOT perfect

--- home page/src.js
+++ home page/src.js
@@ -5,39 +5,83 @@ function getLetterIds() {
     window.alert(object["A"]);
 }
 
-let opened = "closed";
-
-// this function opens the scissors
-function openScissors() {
+// this function turns the scissors
+function turnScissors() {
     let top = document.getElementById("top");
     let bottom = document.getElementById("bottom");
     if (opened == "closed") {
         top.animate (
             {
-                transform: ["rotate(0deg)", "rotate(10deg)"]
+                transform: ["rotate(0deg)", "rotate(5deg)"]
             }, 1000,
         )
         bottom.animate (
             {
-                transform: ["rotate(0deg)", "rotate(10deg)"]
+                transform: ["rotate(0deg)", "rotate(5deg)"]
             }, 1000,
         )
-        top.style.transform = "rotate(10deg)";
-        bottom.style.transform = "rotate(10deg)";
+        top.style.transform = "rotate(5deg)";
+        bottom.style.transform = "rotate(5deg)";
         opened = "opened";
     } else {
         top.animate (
             {
-                transform: ["rotate(10deg)", "rotate(0deg)"]
+                transform: ["rotate(5deg)", "rotate(0deg)"]
             }, 1000,
         )
         bottom.animate (
             {
-                transform: ["rotate(10deg)", "rotate(0deg)"]
+                transform: ["rotate(5deg)", "rotate(0deg)"]
             }, 1000,
         )
         top.style.transform = "rotate(0deg)";
         bottom.style.transform = "rotate(0deg)";
         opened = "closed";
     }
+}
+
+let opened = "closed";
+
+function openScissors() {
+    let top = document.getElementById("top");
+    let bottom = document.getElementById("bottom");
+    let menu = document.getElementById("menu");
+    let expanded_menu = document.getElementById("expanded_menu");
+    if (opened == "closed") {
+        top.animate (
+            {
+                transform: ["rotate(0deg)", "rotate(20deg)"]
+            }, 1000,
+        )
+        bottom.animate (
+            {
+                transform: ["rotate(0deg)", "rotate(-20deg)"]
+            }, 1000,
+        )
+        menu.animate (
+            {
+                visibility: ["visible", "hidden"]
+            }, 10,
+        )
+        expanded_menu.animate (
+            {
+                visibility: ["hidden", "visible"]
+            }, 5000,
+        )
+
+        
+        top.style.transform = "rotate(20deg)";
+        bottom.style.transfrom = "rotate(-20deg)";
+        menu.style.visibility = "hidden";
+        expanded_menu.style.visibility = "visible";
+
+        // menu.style.visibility = "hidden";
+        // menu.src = "../_archive/menu/full_menu.png";
+        // menu.style.height = "30vh";
+        // menu.style.visibility = "visible";
+    }
+}
+
+function sayhi() {
+    window.alert("hello");
 }
