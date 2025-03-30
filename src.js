a6ac3906ce17c7321c0c632828ff9b27c506283e
mneedham01 <mneedham@smith.edu>
2025-03-30 15:18:21 -0400

    added another version of home page

---
+++ src.js
@@ -0,0 +1,7 @@
+function expand_menu(){
+    let menu = document.getElementById("menu");
+    menu.style.visibility = "hidden";
+
+    let expanded_menu = document.getElementById("expanded_menu");
+    expanded_menu.style.visibility = "visible";
+}
