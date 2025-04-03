8680a9f37057145e79f2481c948784272ff7bbdf
buddy <kluong@smith.edu>
2025-04-03 13:17:38 -0400

    book viewer edits

--- archives page/src.js
+++ archives page/src.js
@@ -51,7 +51,7 @@ function goNextPage() {
                 closeBook(false);
                 break;
             default:
-                throw new Error("unkown state");
+                throw new Error("unknown state");
         }
         currentLocation++;
     }
@@ -119,11 +119,11 @@ function openViewer(){
 //     x.style.display = "none"
 // }
 //
-// function openViewer(iD, elmtID){
-//     var elmt = document.getElementById(elmtID)
-//     var x = document.getElementById(iD);
-//     x.style.display = "block"
-//
-// }
+function openViewer(iD, elmtID){
+    var elmt = document.getElementById(elmtID)
+    var x = document.getElementById(iD);
+    x.style.display = "flex"
+
+}
 
 
