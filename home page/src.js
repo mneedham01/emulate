0aba8f317e7b1be38c649abd0e3bcddbba20b351
kzhao26 <kzhao@smith.edu>
2025-04-10 21:42:10 -0400

    scissors animation edit

--- home page/src.js
+++ home page/src.js
@@ -101,4 +101,9 @@ function openScissors() {
     }
 }
 
-
+randomize('doodle')
+const getRandom = (min, max) => Math.floor(Math.random()*(max-min+1)+min);
+function randomize(ID){
+    ID.style.left = getRandom(0, 100)+'%';
+    ID.style.top = getRandom(0, 100)+'vh';
+}
