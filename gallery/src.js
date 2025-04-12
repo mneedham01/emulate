f78793f8a2fb4e0ddd9708a1d9adea0b73138fb1
mneedham01 <mneedham@smith.edu>
2025-04-12 15:45:05 -0400

    attempts to change the pic loading into a while loop (not working)

--- gallery/src.js
+++ gallery/src.js
@@ -1,18 +1,41 @@
+// this function is called when the button for a particular event is pressed 
 function populate(id){
+    // first, change the color of the button to indicate it has been pressed 
     document.getElementById(id).style.backgroundColor = "#27293D";
-    // location.reload();
-    for(let i=1; i < 26; i++){
+    // loop through the folder until there are no more files 
+    var valid_address = true; 
+    // establish the first side to be left
+    var left_column = true; 
+    // establish the first address to be 1 
+    var i = 1; 
+    while (valid_address) {
         let img = document.createElement('img');
-        img.id = "img" + i;
-        // document.getElementById("img" + i).addEventListener("click", zoom());
-        img.src = '../_archive/eventPhotos/' + id + '/' + i + '.JPG';
-        if(i < 26/2) {
+        // give the image a source 
+        img.src = '../_archive/eventPhotos/' + id + '/' + i + '.jpg';
+        // if there is an error (if we have run out of photos), then
+        img.onLoad = onLoad();
+        
+        img.onError = errorHandler();
+        
+        // errorHandler function 
+        function errorHandler() {
+            // alert("hello");
+            valid_address = false; 
+        }
+
+        // place in appropriate column
+        if (left_column) {
             document.getElementById("col1").appendChild(img);
         } else {
             document.getElementById("col2").appendChild(img);
         }
+        // increase the id 
+        i++; 
+        // switch sides 
+        left_column = false;
     }
 }
+
 function refresh(){
     let img;
     for (let i = 1; i < 26; i++) {
