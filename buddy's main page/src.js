23bc407fc002e623e012ca3c01f467f3d1352344
buddy <kluong@smith.edu>
2025-04-17 13:20:16 -0400

    working on a possible homepage design

--- buddy's main page/src.js
+++ buddy's main page/src.js
@@ -8,83 +8,52 @@ function expand_menu(){
     let svg = document.getElementById("expanded_menu");
     svg.style.visibility = "visible";
 }
+// function getImages(){
+//     let images = []
+//
+//     let letters = ["A", "E", "G", "I", "L", "M", "N", "T", "U"]
+//     let src=""
+//     for(let i=0; i< letters.length; i++){
+//         for(let j=1; j < 12; j++){
+//             src = "../_archive/letters/" + letters[i] + j + ".png"
+//             images.append(src)
+//         }
+//     }
+//     return images
+// }
+// const images = getImages();
 
-function setup() {
-    let cnv = createCanvas(windowWidth, windowHeight);
-    cnv.parent("canvas-parent");
-    cnv.style("display", "block");
-    cnv.style("position", "absolute");
-    cnv.style("inset", "0");
-    cnv.style("z-index", "-1");
-    lastMousePos = { x: mouseX, y: mouseY };
+const displayDistance = 100 // distance in px to display another photo
+const nDisplay = 7 // number of pictures to display at once
+
+const images = document.getElementsByClassName("image")
+
+let globalIndex = 0 // used to count up the images
+let lastMousePosition = {x: 0, y: 0} // used to get the last mouse position
+
+// function to activate photos
+function activatePic(img, x, y){
+    img.style.display = "block"
+    img.style.left = `${x}px`
+    img.style.top = `${y}px`
+    img.style.zIndex = globalIndex // otherwise the last pic will always be at the top
+    lastMousePosition = {x: x, y: y} // update the last mouse position
 }
 
-let distThreshold = 100;
-// scale factor to size images
-let scaleFactor = 4;
-
-// VARIABLES
-// array to hold all of our images
-let images = [];
-// array to hold history of mouse positions and image index for that position
-let queue = [];
-// object containing our last (stored) mouse position
-let lastMousePos = { x: 0, y: 0 };
-
-for(int)
-function preload() {
-    for (let i = 0; i < imageUrls.length; i++) {
-        images[i] = loadImage(imageUrls[i]);
-    }
+// compute mouse distance
+function mouseDistance(x, y){
+    return Math.hypot(x - lastMousePosition.x, y - lastMousePosition.y)
 }
 
-function draw() {
-    // clear the canvas
-    clear();
-    background(41, 41, 41);
+// onmousemove
+window.onmousemove = e => {
+    if (mouseDistance(e.clientX, e.clientY) > displayDistance){
+        let activePic = images[globalIndex % images.length]
+        // let inactivePic = images[(globalIndex - nDisplay) % images.length]
 
-    // calculate distance between current mouse position and last stored mouse position.
-    let d = dist(mouseX, mouseY, lastMousePos.x, lastMousePos.y);
+        activatePic(activePic, e.clientX, e.clientY)
+        // if (inactivePic){inactivePic.dataset.status = "inactive"}
 
-    // If distance is greater than threshold:
-    // 1. Add current mouse position and current image index to the front of the queue
-    // 2. Update lastMousePos to current mouse position
-    // 3. Update imgIndex to next image index
-    if (d > distThreshold) {
-        queue.unshift({ x: mouseX, y: mouseY, index: imgIndex });
-        lastMousePos = { x: mouseX, y: mouseY };
-        imgIndex = (imgIndex + 1) % images.length;
+        globalIndex++
     }
-
-    // maintain queue length equal to number of images by removing the last item
-    if (queue.length > images.length) {
-        queue.pop();
-    }
-
-    // define scale of images based on width of canvas
-    let scale = width / scaleFactor;
-
-    // draw images in queue
-    // draw order is reversed so that the first image in the queue is drawn on top
-    for (let i = queue.length - 1; i >= 0; i--) {
-        let img = images[queue[i].index];
-        if (img) {
-            // scale image based on scale factor
-            let imgWidth = (img.width * scale) / img.width;
-            let imgHeight = (img.height * scale) / img.width;
-            // draw image at mouse position offset by half of image width and height
-            image(
-                img,
-                queue[i].x - imgWidth / 2,
-                queue[i].y - imgHeight / 2,
-                imgWidth,
-                imgHeight
-            );
-        }
-    }
-}
-
-// resize canvas when window is resized
-function windowResized() {
-    resizeCanvas(windowWidth, windowHeight);
 }
