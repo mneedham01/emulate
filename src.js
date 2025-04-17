9fb416bc44dd354cc9e0252bba5e891c72a8e555
mneedham01 <mneedham@smith.edu>
2025-04-17 17:16:07 -0400

    added open/close function

--- src.js
+++ src.js
@@ -1,7 +1,25 @@
+var expanded = false; 
+function menu() {
+    if (expanded) {
+        shrink_menu();
+        expanded = false; 
+    } else {
+        expand_menu();
+        expanded = true; 
+    }
+}
 function expand_menu(){
     let menu = document.getElementById("menu");
     menu.style.visibility = "hidden";
 
     let expanded_menu = document.getElementById("expanded_menu");
     expanded_menu.style.visibility = "visible";
+}
+
+function shrink_menu(){
+    let menu = document.getElementById("menu");
+    menu.style.visibility = "visible";
+
+    let expanded_menu = document.getElementById("expanded_menu");
+    expanded_menu.style.visibility = "hidden"; 
 }
